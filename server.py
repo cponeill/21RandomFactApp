31c1d0b084f0051b21faa50c2ede2c09bb7fd3e9
Casey O'Neill <caseyoneill78@hotmail.com>
2016-02-29 18:19:27 -0800

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 # loading developer libraries
-import requests, json, yaml
+import requests, json, urllib, yaml
 from flask import Flask, request
 from two1.lib.wallet import Wallet
 from two1.lib.bitserv.flask import Payment
@@ -20,12 +20,12 @@ def send_random_fact():
 
   Learn more about this API ---> http://numbersapi.com
   '''
-  
+
   # Formatting the variable "url" to output JSON.
   response = requests.get(url)
   year_json = json.loads(response.text)
-  
-  # Return the year and text data from the json output and display it in the command line. 
+
+  # Return the year and text data from the json output and display it in the command line.
   if len(year_json) == 0:
     return "There are no facts at all."
   else:
@@ -37,20 +37,18 @@ def send_random_fact():
 @app.route('/manifest')
 def docs():
   '''
-  Provides the app manifest to the 21 crawler.
+  Provides the app manifest to the 21 crawler
   '''
   with open('manifest.yaml', 'r') as f:
     manifest_yaml = yaml.load(f)
   return json.dumps(manifest_yaml)
 
-
 @app.route('/client')
 def client():
-  '''
-  Provides an example client script.
-  '''
-  return send_from_directory('static', 'client.py')
+
+  return send_from_directory('~/MyApp/21RandomFactApp/static', 'client.py')
+
 
 
 if __name__=='__main__':
-  app.run(host='10.147.17.138', debug=True)
+  app.run(host='0.0.0.0', debug=True)
