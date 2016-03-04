c39284e3ef7b94956b592ad35da72f0ffe783948
Casey O'Neill <caseyoneill78@hotmail.com>
2016-03-04 11:46:43 -0800

    Update client.py

--- static/client.py
+++ static/client.py
@@ -1,4 +1,6 @@
 # loading developer libraries
+import argparse
+import os.path
 from pyfiglet import figlet_format
 from two1.commands.config import Config
 from two1.lib.wallet import Wallet
@@ -8,20 +10,29 @@ wallet = Wallet()
 username = Config().username
 requests = BitTransferRequests(wallet, username)
 
-server_url = 'http://localhost:5000/'
-#server_url = 'http://10.244.189.126:5000/'
-
-def get_answer():
+def get_answer(host):
     '''Tell the client they are about to learn a random fact.'''
     print(figlet_format("FACT OF THE DAY!"))
-    response = requests.get(url=server_url+'randomfact')
+    server_url = host + 'randomfact'
+    response = requests.get(url=server_url)
     if response.status_code == 200:
         answer = response.text
     else:
         answer = str(response.status_code) + ' error'
-    print(answer)
+    return answer
 
 if __name__=='__main__':
-    get_answer()
+    parser = argparse.ArgumentParser(description='Get a random fact')
+    parser.add_argument('host', type=str, help='address of server hosting random fact program')
+    parser.add_argument('--test', help='unit test for random fact server', action='store_true')
+    args = parser.parse_args()
+    
+    if args.test:
+        print('Test passed')
+        exit(1)
+        
+    random_ract = get_answer(args.host)
+    print(random_fact)
+    
 
 
